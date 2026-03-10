542f6c85a7070159597a8d0430a5f2fed5094e2d
Caizhenxin <Caizhenxin@users.noreply.github.com>
2026-03-10 11:02:47 +0000

    deploy: 357d760f33eb18fe440cac85a46fa322ec6a3e3e

---
+++ assets/post.C2jW3clt.js
@@ -0,0 +1,1 @@
+import"./chunks/vue-i18n.CTk8maKi.js";import{u as t}from"./index.CLyhDqjj.js";import{w as e}from"./framework.U0K-pJOS.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
