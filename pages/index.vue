64435e4cab19f6abf1a07ee25359932b11082a0a
蔡振辛 <137786537+Caizhenxin@users.noreply.github.com>
2026-03-11 19:47:51 +0800

    feat: 自定义布局和页面重构

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home-page">
+    <YunBanner />
+    <YunPostList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import YunBanner from 'valaxy-theme-yun/components/YunBanner.vue'
+import YunPostList from 'valaxy-theme-yun/components/YunPostList.vue'
+</script>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
