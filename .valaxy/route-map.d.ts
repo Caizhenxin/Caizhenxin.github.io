311b50ef3a425f8ce1d856dbcf887a98a44f4f5b
蔡振辛 <137786537+Caizhenxin@users.noreply.github.com>
2026-04-02 17:24:20 +0800

    Update route-map.d.ts

--- .valaxy/route-map.d.ts
+++ .valaxy/route-map.d.ts
@@ -102,7 +102,7 @@ declare module 'vue-router/auto-routes' {
    * @internal
    */
   export interface _RouteFileInfoMap {
-    'node_modules/.pnpm/valaxy-theme-yun@0.27.0_vue@3.5.29_typescript@5.9.3_/node_modules/valaxy-theme-yun/pages/index.vue': {
+    'pages/index.vue': {
       routes:
         | '/'
       views:
