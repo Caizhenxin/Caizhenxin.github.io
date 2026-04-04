9ffa271681305866d7c172dd80c6c54eb8484228
Caizhenxin <Caizhenxin@users.noreply.github.com>
2026-04-04 05:03:34 +0000

    deploy: a65f54d869f0ed35e7ee8efc2ba64c3a94c1b129

--- assets/back.CDRNMY7k.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./chunks/vue-router.U9Gvnj2c.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
